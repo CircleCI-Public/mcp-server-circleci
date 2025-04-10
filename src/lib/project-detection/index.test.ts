1c925de4a4b558dc4a42f9d75f4911d01eb9bfff
Joel Thompson <joel.thompson@circleci.com>
2025-04-10 15:37:12 -0700

    chore: add extra test cases

--- src/lib/project-detection/index.test.ts
+++ src/lib/project-detection/index.test.ts
@@ -3,10 +3,12 @@ import { describe, it, expect } from 'vitest';
 
 describe('getPipelineNumberFromURL', () => {
   it.each([
+    // Workflow URL
     {
       url: 'https://app.circleci.com/pipelines/gh/organization/project/2/workflows/abc123de-f456-78gh-90ij-klmnopqrstuv',
       expected: 2,
     },
+    // Workflow URL
     {
       url: 'https://app.circleci.com/pipelines/circleci/GM1mbrQEWnNbzLKEnotDo4/5gh9pgQgohHwicwomY5nYQ/123/workflows/abc123de-f456-78gh-90ij-klmnopqrstuv',
       expected: 123,
@@ -18,18 +20,42 @@ describe('getPipelineNumberFromURL', () => {
 
 describe('getProjectSlugFromURL', () => {
   it.each([
+    // Workflow URL
     {
       url: 'https://app.circleci.com/pipelines/gh/organization/project/2/workflows/abc123de-f456-78gh-90ij-klmnopqrstuv',
       expected: 'gh/organization/project',
     },
+    // Workflow URL
     {
       url: 'https://app.circleci.com/pipelines/circleci/GM1mbrQEWnNbzLKEnotDo4/5gh9pgQgohHwicwomY5nYQ/123/workflows/abc123de-f456-78gh-90ij-klmnopqrstuv',
       expected: 'circleci/GM1mbrQEWnNbzLKEnotDo4/5gh9pgQgohHwicwomY5nYQ',
     },
+    // Pipeline URL
+    {
+      url: 'https://app.circleci.com/pipelines/gh/organization/project/123',
+      expected: 'gh/organization/project',
+    },
+    // Pipeline URL
+    {
+      url: 'https://app.circleci.com/pipelines/circleci/GM1mbrQEWnNbzLKEnotDo4/5gh9pgQgohHwicwomY5nYQ/456',
+      expected: 'circleci/GM1mbrQEWnNbzLKEnotDo4/5gh9pgQgohHwicwomY5nYQ',
+    },
+    // Job URL
+    {
+      url: 'https://app.circleci.com/pipelines/gh/organization/project/2/workflows/abc123de-f456-78gh-90ij-klmnopqrstuv/jobs/xyz789',
+      expected: 'gh/organization/project',
+    },
+    // Job URL
+    {
+      url: 'https://app.circleci.com/pipelines/circleci/GM1mbrQEWnNbzLKEnotDo4/5gh9pgQgohHwicwomY5nYQ/123/workflows/abc123de-f456-78gh-90ij-klmnopqrstuv/jobs/def456',
+      expected: 'circleci/GM1mbrQEWnNbzLKEnotDo4/5gh9pgQgohHwicwomY5nYQ',
+    },
+    // Project URL
     {
       url: 'https://app.circleci.com/pipelines/gh/organization/project',
       expected: 'gh/organization/project',
     },
+    // Project URL
     {
       url: 'https://app.circleci.com/pipelines/circleci/GM1mbrQEWnNbzLKEnotDo4/5gh9pgQgohHwicwomY5nYQ',
       expected: 'circleci/GM1mbrQEWnNbzLKEnotDo4/5gh9pgQgohHwicwomY5nYQ',
